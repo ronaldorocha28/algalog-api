19cf93c44fb99bc1104b6d64e959a378003143a8
Ronaldo Rocha <ronaldo.rocha28@hotmail.com>
2022-03-10 16:22:49 -0300

    Implementando Collection Resource

---
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -0,0 +1,14 @@
+package com.algaworks.algalog.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+}
