3b000d6bf0f4dec3c8ac34b00abed51c8764879f
Ronaldo Rocha <ronaldo.rocha28@hotmail.com>
2022-03-15 16:49:00 -0300

    Implementando Jakarta Persistence JPA

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,21 +12,14 @@ import com.algaworks.algalog.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
-	
+
+	@PersistenceContext
+	private EntityManager manager;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setName("João");
-		cliente1.setEmail("joao@gmail.com");
-		cliente1.setPhone("99 9999-9999");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setName("Maria");
-		cliente2.setEmail("maria@gmail.com");
-		cliente2.setPhone("88 8888-8888");
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 
 }
