d32cf8a44f060add31e37d65578cfaebfe84206e
Ronaldo Rocha <ronaldo.rocha28@hotmail.com>
2022-03-15 18:18:14 -0300

    Usando Spring Data JPA

---
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
@@ -0,0 +1,19 @@
+package com.algaworks.algalog.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algalog.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+	List<Cliente> findByEmail(String email);
+	List<Cliente> findByEmailContaining(String email);
+
+
+}
